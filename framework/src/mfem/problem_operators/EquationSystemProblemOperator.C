178ccd5ba74822aabdc1bc11e3cd261502c577bb
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2026-04-08 10:57:56 +0100

    Post-rebase fixes

--- framework/src/mfem/problem_operators/EquationSystemProblemOperator.C
+++ framework/src/mfem/problem_operators/EquationSystemProblemOperator.C
@@ -38,13 +38,12 @@ EquationSystemProblemOperator::Solve()
   GetEquationSystem()->FormLinearSystem(_true_x, _true_rhs);
 
   if (_problem_data.jacobian_solver->isLOR() && GetEquationSystem()->GetTestVarNames().size() > 1)
-    mooseError("The LOR method is only supported for single-variable systems");
-
+    mooseError("LOR solve is only supported for single-variable systems");
   _problem_data.jacobian_solver->updateSolver(
       *GetEquationSystem()->_blfs.Get(GetEquationSystem()->GetTestVarNames().at(0)),
       GetEquationSystem()->_ess_tdof_lists.at(0));
-    
-  _problem_data.nonlinear_solver->SetSolver(_problem_data.jacobian_solver->getSolver());
+
+  _problem_data.nonlinear_solver->SetPreconditioner(_problem_data.jacobian_solver->getSolver());
   _problem_data.nonlinear_solver->SetOperator(*GetEquationSystem());
   _problem_data.nonlinear_solver->Mult(_true_rhs, _true_x);
   GetEquationSystem()->SetTrialVariablesFromTrueVectors(_true_x);
